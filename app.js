603550ae0ee85807c3e75c251c06bedd75e2faad
Kapil Kumar <ks22gg@gmail.com>
2024-07-15 14:08:03 +0530

    game

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let boxes = document.querySelectorAll(".box");
-let resetBtn = document.querySelector("reset-btn");
+let resetBtn = document.querySelector("#reset-btn");
 let newGameBtn = document.querySelector("#new-btn");
 let msgContainer = document.querySelector(".msg-container");
 let msg = document.querySelector("#msg");
